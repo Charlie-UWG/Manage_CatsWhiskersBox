1f013d149a35a0e4646d029cdfa9866fff982ed1
Charlie-UWG <75844445+Charlie-UWG@users.noreply.github.com>
2021-02-22 16:13:26 +0900

    mock

--- src/App.js
+++ src/App.js
@@ -10,13 +10,56 @@ export const App = () => {
       <div class="dateInputDiv">
         <label>受注日入力</label>
         <input
-          placeholder="受注日"
+          // placeholder="受注日"
           type="date"
           value={orderDate}
           onChange={onChangeOrderDate}
         />
       </div>
-      <div class="orderInputDiv"></div>
+      <div class="orderInputDiv">
+        <button>猫のひげ</button>
+        <ul>
+          <li>
+            <p>1</p>
+            <div>Image</div>
+            <p>ミケ</p>
+            <div>
+              <label>注文数</label>
+              <input type="number" />
+            </div>
+          </li>
+          <li>
+            <p>2</p>
+            <div>Image</div>
+            <p>キジ</p>
+            <div>
+              <label>注文数</label>
+              <input type="number" />
+            </div>
+          </li>
+        </ul>
+        <button>うさぎのひげ</button>
+        <ul>
+          <li>
+            <p>1</p>
+            <div>Image</div>
+            <p>ネザーランドドワーフ</p>
+            <div>
+              <label>注文数</label>
+              <input type="number" />
+            </div>
+          </li>
+          <li>
+            <p>2</p>
+            <div>Image</div>
+            <p>ロップイヤー</p>
+            <div>
+              <label>注文数</label>
+              <input type="number" />
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
