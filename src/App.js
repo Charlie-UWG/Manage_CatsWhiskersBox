3586d6043ba7db7d8e4a02f694bc2e966c5a6168
Charlie-UWG <75844445+Charlie-UWG@users.noreply.github.com>
2021-02-23 12:05:37 +0900

    受注入力欄にmap関数を使用 注文数をそれぞれのitemに対応させる処理が必要

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export const App = () => {
   const [orderDate, setOrderDate] = useState("");
   const onChangeOrderDate = (event) => setOrderDate(event.target.value);
 
+  const [buttonNames, setButtonNames] = useState(["猫のひげ",　"ウサギのひげ"]);
+
+  const itemNames = ["ミケ","キジ","ノビノビ","ジャンプ"];
+
   return (
     <div>
       <div class="dateInputDiv">
@@ -17,25 +21,30 @@ export const App = () => {
         />
       </div>
       <div class="orderInputDiv">
-        <button>猫のひげ</button>
-        <ul>
-          <li class="orderRow">
-            <div class="illst mr-10">Image</div>
-            <p class="wd-100 mr-10">ミケ</p>
-            <div>
-              <label>注文数</label>
-              <input type="number" />
-            </div>
-          </li>
-          <li class="orderRow">
-            <div class="illst mr-10">Image</div>
-            <p class="wd-100 mr-10">キジ</p>
-            <div>
-              <label>注文数</label>
-              <input type="number" />
-            </div>
-          </li>
-        </ul>
+        {buttonNames.map((buttonName) => {
+          return (
+            <>
+              <button key={buttonName} >{buttonName}</button>
+              <ul>
+                {itemNames.map((item) => {
+                  return (
+                    <>
+                    <li class="orderRow">
+                      <div class="illst mr-10">Image</div>
+                      <p class="wd-100 mr-10">{item}</p>
+                      <div>
+                        <label>注文数</label>
+                        <input type="number" />
+                      </div>
+                    </li>
+                    </>
+                  )
+                })}
+              </ul>
+            </>
+          );
+        })}
+
         <button>うさぎのひげ</button>
         <ul>
           <li class="orderRow">
